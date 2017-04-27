7c59583320475b6e515cbcdefcb52e4eb1fd0789
Carlos Araújo <carlos.araujo@concrete.com.br>
2017-04-27 14:37:56 -0300

    Add lib/ para o funcionamento do método DELETE dentro do test.js, essa funcionalidade ainda esta em implementação

--- test.js
+++ test.js
@@ -1,14 +1,26 @@
 'use strict';
 
+var expect = require('chai').expect;
 var assert = require('chai').assert;
 var request = require('supertest');
-var expect = require('chai').expect;
-
 var url = 'http://localhost:5000';
 
+var db = require('../lib/db');
+var app = require('../lib/index');
+var mongojs = require('mongojs');
+var ObjectId = mongojs.ObjectId;
+var _conn;
+
+var fullContact = {
+  '_id': ObjectId('5901f69132f9d935ec1115a3'),
+  'name': 'QA CS',
+  'mobilephone': '0551185087777',
+  'homephone': '0551141409999'
+};
+
 var contatoCompleto = {
-  'name': 'Romero',
-  'mobilephone': '0551199995555',
+  'name': 'Zidane',
+  'mobilephone': '0551185084469',
   'homephone': '0551141394567'
 };
 
@@ -21,10 +33,33 @@ describe('Teste na API PhoneBook - método: POST', function() {
       .end(function(err, res) {
         var result = JSON.parse(res.text);
         assert.equal(res.status, 201);
-        assert.equal(result.name, 'Romero', 'Conferindo o name!');
-        assert.equal(result.mobilephone, '0551199995555', 'Conferindo o mobilephone!');
+        assert.equal(result.name, 'Zidane', 'Conferindo o name!');
+        assert.equal(result.mobilephone, '0551185084469', 'Conferindo o mobilephone!');
         assert.equal(result.homephone, '0551141394567', 'Conferindo o homephone!');
         done();
       });
+  });
+});//describe
+
+describe('Teste na API PhoneBook - DELETE ', function() {
+  before(function(done) {
+    db.then(function(conn) {
+      _conn = conn;
+      done();
+    });
+  });
+
+  after(function(done) {
+    _conn.contacts.remove({}, function(err, res) {
+      done();
+    });
+  });
+
+  it('Excluindo um contato via ID.', function(done) {
+    _conn.contacts.insert(fullContact, function(err, res) {
+      request(app)
+        .delete('/contacts/56d5efa8c82593800291c02b')
+      done();
+    });
   });
 });
